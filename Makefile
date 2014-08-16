27ef3e7de4353e1b23b46e4821d7320053c6d0c2
Strick Yak <strickyak@gmail.com>
2014-08-15 20:38:34 -0700

    work on laph

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ _rpc:
 	python ../rye/rye.py run rpc.py
 
 _laph:
-	python ../rye/rye.py run laph.py
+	python ../rye/rye.py run laph_test.py laph.py
 
 _dns_ask:
 	python ../rye/rye.py run dns_ask.py
--- laph.py
+++ laph.py
@@ -1,15 +1,6 @@
-
-#from go import fmt
-#from go import html
-#from go import io/ioutil
-#from go import net/http
-#from go import net/url
 from go import strings
 from go import regexp
-#from go import time
 
-WORD = regexp.MustCompile('([A-Za-z])([A-Za-z0-9_]+)')
-WORDS = regexp.MustCompile('(([A-Za-z])([A-Za-z0-9_]+))([.](([A-Za-z])([A-Za-z0-9_]+))*)')
 PARENT = regexp.MustCompile('([A-Za-z0-9_.]+)[.]([A-Za-z0-9_]+)')
 
 FRONT_WHITE = regexp.MustCompile('^(\\s+)')
@@ -28,19 +19,12 @@ L2 = [ ('Open', FRONT_OPEN), ('Close', FRONT_CLOSE), ('Quote', FRONT_QUOTE) ]
 L3 = [ ('Eq', FRONT_EQ), ('Words', FRONT_WORDS), ('Header', FRONT_HEADER) ]
 LEXERS = L1 + L2 + L3
 
-print "word", WORD
-print WORD.Find("345 Foo777 bar ")
-print WORD.FindString("345 Foo777 bar ")
-print WORD.FindAllString("345 Foo777 bar ", -1)
-print WORD.FindAllStringIndex("345 Foo777 bar ", -1)
-print 'last', WORD.FindAll("345 Foo777 bar ", -1)
-
-print 'FRONT_HEADER.FindString', FRONT_HEADER.FindString('[abc] xxx')
 assert FRONT_HEADER.FindString('[abc] xxx') == '[abc]'
-
-assert WORDS.FindString('FooBar.Baz')
 assert not FRONT_WORDS.FindString(' FooBar.Baz')
-print 'OKAY'
+
+QUOTE0 = regexp.MustCompile('^["]')
+QUOTE1 = regexp.MustCompile('["]$')
+QUOTE2 = regexp.MustCompile('["]["]')
 
 def Tokenize(text):
   while text:
@@ -63,122 +47,128 @@ def Tokenize(text):
 print 'Tokenize:', list(Tokenize(' [Lyric] we_re up=all.night2 == get_lucky '))
 
 class Stanza:
-  def __init__(self, engine):
-    self.name = None
-    self.up = None
-    self.slots = {}
-    self.engine = engine
+  def __init__(engine):
+    .name = None
+    .up = None
+    .slots = {}
+    .engine = engine
 
-  def Lookup(self, key):
-    z = self.slots.get(key)
+  def Lookup(key):
+    z = .slots.get(key)
     if z:
       return z
     else:
-      if self.slots.up:
-        return self.slots.up.Lookup(key)
+      if .slots.up:
+        return .slots.up.Lookup(key)
       else:
         return None
 
 class Engine:
-  def __init__(self, text):
-    self.stanzas = {}
-    self.text = text
-    self.toks = list(Tokenize(text))
-    self.n = len(self.toks)
-    self.p = -1
-    self.Advance()
+  def __init__(text):
+    .stanzas = {}
+    .text = text
+    .toks = list(Tokenize(text))
+    .n = len(.toks)
+    .p = -1
+    .Advance()
 
-  def Advance(self):
-    if self.p >= self.n:
-      self.t = None
-      self.v = None
+  def Advance():
+    if .p >= .n:
+      .t = None
+      .v = None
       return
 
-    self.p += 1
-    if self.p >= self.n:
-      self.t = None
-      self.v = None
+    .p += 1
+    if .p >= .n:
+      .t = None
+      .v = None
       return
-    self.t = self.toks[self.p][0]
-    self.v = self.toks[self.p][1]
+    .t = .toks[.p][0]
+    .v = .toks[.p][1]
 
-  def MakeStanza(self, s):
-    sp = self.stanzas.get(s)
+  def MakeStanza(s):
+    sp = .stanzas.get(s)
     if not sp:
       sp = Stanza(self)
       sp.name = s
       m = PARENT.FindStringSubmatch(s)
-      self.stanzas[sp.name] = sp
+      .stanzas[sp.name] = sp
       if m:
         print 'm: ', m
-        sp.up = self.MakeStanza(m[1])
+        sp.up = .MakeStanza(m[1])
     return sp
 
-  def Parse(self):
-    while self.t:
-      self.ParseStanza()
+  def Parse():
+    while .t:
+      .ParseStanza()
     return self
 
-  def ParseStanza(self):
-    self.MustT('Header')
-    name = FRONT_HEADER.FindString(self.v)
+  def ParseStanza():
+    .MustT('Header')
+    name = FRONT_HEADER.FindString(.v)
     name = name[1:-1]  # Trim the brackets.
-    stanza = self.MakeStanza(name)
-    self.Advance()
+    stanza = .MakeStanza(name)
+    .Advance()
 
-    while self.t == 'Words':
-      self.ParseSlot(stanza)
+    while .t == 'Words':
+      .ParseSlot(stanza)
 
-  def ParseSlot(self, stanza):
-    k = self.v
-    self.Advance()
+  def ParseSlot(stanza):
+    k = .v
+    .Advance()
 
-    self.MustT('Eq')
-    self.Advance()
+    .MustT('Eq')
+    .Advance()
 
-    v = self.ParseExpr()
+    v = .ParseExpr()
     stanza.slots[k] = v
-    self.Advance()
+    .Advance()
 
-  def ParseExpr(self):
-    if not self.t:
+  def ParseExpr():
+    if not .t:
       raise "Expected expression but got End of String."
-    if self.t == 'Open':
-      return self.ParseList()
-    if self.t == 'Words':
-      z = Intern(str(self.v))
-      self.Advance()
+    if .t == 'Open':
+      return .ParseList()
+    if .t == 'Words':
+      z = Intern(str(.v))
+      .Advance()
       return z
-    if self.t == 'Int':
-      z = Lit(int(self.v))
-      self.Advance()
+    if .t == 'Int':
+      z = Lit(int(.v))
+      .Advance()
       return z
-    if self.t == 'Float':
-      z = Lit(float(self.v))
-      self.Advance()
+    if .t == 'Float':
+      z = Lit(float(.v))
+      .Advance()
       return z
-    if self.t == 'Str':
-      z = Lit(str(self.v))
-      self.Advance()
+    if .t == 'Str':
+      say 'dog', .v
+      say 'dog', str(.v)
+      s = .v
+      s = QUOTE0.ReplaceAllString(s, '')
+      s = QUOTE1.ReplaceAllString(s, '')
+      s = QUOTE2.ReplaceAllString(s, '')
+      z = Lit(str(s))
+      .Advance()
       return z
-    if self.t == 'Quote':
-      self.Advance()
-      x = self.ParseExpr()
+    if .t == 'Quote':
+      .Advance()
+      x = .ParseExpr()
       return List([Intern('quote'), x])
-    raise 'ParseExpr unknown: %s: %d' % (self.t, self.v)
+    raise 'ParseExpr unknown: %s: %d' % (.t, .v)
 
-  def ParseList(self):
-    self.MustT('Open')
-    self.Advance()
+  def ParseList():
+    .MustT('Open')
+    .Advance()
     z = []
-    while self.t != 'Close':
-      z.append(self.ParseExpr())
+    while .t != 'Close':
+      z.append(.ParseExpr())
     return List(z)
 
 
-  def MustT(self, t):
-    if self.t != t:
-      self.Bad('Expected %s, got %s: %s' % (t, self.t, self.v))
+  def MustT(t):
+    if .t != t:
+      .Bad('Expected %s, got %s: %s' % (t, .t, .v))
 
 Interned = {}
 
@@ -198,62 +188,62 @@ T = Intern("true")
 F = Intern("false")
 
 class Node:
-  def __init__(self):
+  def __init__():
     pass
-  def Eq(self, a):
+  def Eq(a):
     return self is a
 
 class Lit(Node):
-  def __init__(self, x):
-    self.x = x
-  def Eq(self, a):
-    return self.x == a.x if type(a) is Lit else False
-  def Show(self):
-    if type(self.x) == str:
-      return '"%s"' % self.x  # TODO: fix for escaping.
+  def __init__(x):
+    .x = x
+  def Eq(a):
+    return .x == a.x if type(a) is Lit else False
+  def Show():
+    if type(.x) == str:
+      return '"%s"' % .x  # TODO: fix for escaping.
     else:
-      return repr(self.x)
-  def Eval(self, env, stanza):
+      return repr(.x)
+  def Eval(env, stanza):
     return self
 
 class Symbol(Node):
-  def __init__(self, s):
-    self.s = s
-  def Show(self):
-    return self.s
-  def Lookup(self, env, stanza):
+  def __init__(s):
+    .s = s
+  def Show():
+    return .s
+  def Lookup(env, stanza):
     for k, v in env:
-      if self.Eq(k):
+      if .Eq(k):
         return v
     if stanza:
       return stanza.Lookup(self)
-  def Eval(self, env, stanza):
+  def Eval(env, stanza):
     st = stanza
-    ww = strings.Split(self.s, '.')
+    ww = strings.Split(.s, '.')
     for w in ww[:-1]:
       st = st.X
-    return self.Lookup(env, stanza)
+    return .Lookup(env, stanza)
 
 
 class List(Node):
-  def __init__(self, v):
-    self.v = v
+  def __init__(v):
+    .v = v
 
-  def Len(self):
-    return len(self.v)
+  def Len():
+    return len(.v)
 
-  def Show(self):
+  def Show():
     z = '('
-    for x in self.v:
+    for x in .v:
       if len(z) > 1:
         z += ' '
       z += x.Show()
     return z + ')'
 
-  def Eval(self, env, stanza):
-    if len(self.v) < 1:
+  def Eval(env, stanza):
+    if len(.v) < 1:
       return self
-    hd = self.v[0]
+    hd = .v[0]
 
     # Special Forms:
     if hd is Lambda:
@@ -261,54 +251,80 @@ class List(Node):
     if hd is Prim:
       return self
     if hd is Quote:
-      return self.v[1]
+      return .v[1]
 
     cmd = hd.Eval(env, stanza)
-    #say hd
-    #say type(hd)
-    #say type(hd) is List
-    #say type(hd) is Symbol
-    #say type(hd) is Prim
-    if type(hd) is List:
-      if len(hd.v) == 3 and hd.v[0] is Lambda and type(hd.v[1]) is List:
-        formals = hd.v[1]
-        expr = hd.v[2]
-        if len(formals.v) == len(self.v) - 1:
+    if type(cmd) is List:
+      if len(cmd.v) == 3 and cmd.v[0] is Lambda and type(cmd.v[1]) is List:
+        formals = cmd.v[1]
+        expr = cmd.v[2]
+        if len(formals.v) == len(.v) - 1:
           env2 = env
           for i in range(len(formals)):
-            env2 = [(formals[i], hd.v[i+1].Eval(env, stanza))] + env2
+            env2 = [(formals[i], cmd.v[i+1].Eval(env, stanza))] + env2
             return expr.Eval(env2, stanza)
         else:
-          raise 'Wrong number of formals (%s) vs args (%s)' % (len(formals.v), len(self.v) - 1)
+          raise 'Wrong number of formals (%s) vs args (%s)' % (len(formals.v), len(.v) - 1)
 
-      if len(hd.v) == 2 and hd.v[0] is Prim:
-        return PRIMS[hd.v[1].x](self, env, stanza)
+      if len(cmd.v) == 2 and cmd.v[0] is Prim:
+        primName = cmd.v[1].x
+        fn = PRIMS.get(primName)
+        if not fn:
+          raise 'Prim does not exist: %q' % primName
+        return fn(self, env, stanza)
 
-      raise 'Strange list in head position is not valid lambda expr: %s' % hd.v.Show()
-    raise 'Other: %s' % self.Show()
+      raise 'Strange list in head position is not valid lambda expr: %s' % cmd.v.Show()
+    raise 'Other: %s' % .Show()
 
 PRIMS = {}
 
-def PrimPlus(a, env, stanza):
-  return Lit( a.v[1].Eval(env, stanza).x + a.v[2].Eval(env, stanza).x )
+def PrimList(a, env, stanza):
+  b, c = arg2(a, env, stanza)
+  return Lit( b.x + c.x )
 PRIMS['plus'] = PrimPlus
 
-e = Engine(' [Abc] a = "foo" [Def] b = "bar" [Ghi.Xyz] ')
-e.Parse()
-say e.stanzas
-say e.stanzas['Abc'].slots['a']
-z = Engine('[x] y = ( add 34 "23" )').Parse().stanzas['x'].slots['y']
-say z
-say z.Show()
-assert z.Len() == 3, z
+def args(a, env, stanza):
+  say a
+  say a.Show()
+  say a.v
+  tl = a.v[1:]
+  return [x.Eval(env, stanza) for x in tl]
 
-prim_plus__21__2 = List([ List([ Intern('prim'), Lit('plus') ]), Lit(21), Lit(2) ])
-x = prim_plus__21__2.Eval( [], None )
-assert 23 == x.x
-assert type(Prim) is Symbol
-assert type(prim_plus__21__2) is List
-assert type(x) is Lit
-assert type(x.x) is int
-say Symbol
-say str(Symbol)
-say repr(Symbol)
+def arg2(a, env, stanza):
+  assert len(a.v) == 3
+  return args(a, env, stanza)
+
+def PrimPlus(a, env, stanza):
+  b, c = arg2(a, env, stanza)
+  return Lit( b.x + c.x )
+PRIMS['plus'] = PrimPlus
+
+def PrimMinus(a, env, stanza):
+  b, c = arg2(a, env, stanza)
+  return Lit( b.x - c.x )
+PRIMS['minus'] = PrimMinus
+
+def PrimTimes(a, env, stanza):
+  b, c = arg2(a, env, stanza)
+  return Lit( b.x * c.x )
+PRIMS['times'] = PrimTimes
+
+#e = Engine(' [Abc] a = "foo" [Def] b = "bar" [Ghi.Xyz] ')
+#e.Parse()
+#say e.stanzas
+#say e.stanzas['Abc'].slots['a']
+#z = Engine('[x] y = ( add 34 "23" )').Parse().stanzas['x'].slots['y']
+#say z
+#say z.Show()
+#assert z.Len() == 3, z
+#
+#prim_plus__21__2 = List([ List([ Intern('prim'), Lit('plus') ]), Lit(21), Lit(2) ])
+#x = prim_plus__21__2.Eval( [], None )
+#assert 23 == x.x
+#assert type(Prim) is Symbol
+#assert type(prim_plus__21__2) is List
+#assert type(x) is Lit
+#assert type(x.x) is int
+#say Symbol
+#say str(Symbol)
+#say repr(Symbol)
