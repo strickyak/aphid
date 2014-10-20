fdd4bf07c5244e92723319155dd7b522de6e4ec2
strick <strick@localhost>
2014-10-20 09:10:50 -0700

    Selfish DNS: Handle exceptions. Munch flags. Hex dump Q & A.

--- selfish_dns_server.py
+++ selfish_dns_server.py
@@ -1,5 +1,12 @@
 from go import net
 from go import strings
+from . import flag
+from . import hexdump
+
+BUFLEN = 512
+
+ONE_Q_ONE_A = [0, 1, 0, 1, 0, 0, 0, 0]
+LEN_ONE_Q_ONE_A = len(ONE_Q_ONE_A)
 
 def ServeAnswerOnPort(answer, p):
   quad = [int(s) for s in strings.Split(answer, '.')]
@@ -10,27 +17,28 @@ def ServeAnswerOnPort(answer, p):
   conn = net.ListenUDP("udp4", bind)
   conn.SetReadBuffer(4096)
   while True:
+    DoOnce(conn, quad)
+
+def DoOnce(conn, quad):
+  try:
     buf = byt(400)
     say "Reading..."
     n, addr = conn.ReadFromUDP(buf)
-    say n, addr, buf
+    say n, addr
+    hexdump.HexDump(buf[:n], 'QUESTION from %q' % str(addr))
 
-    if buf[2] & 128:  # Question, not response.
-      continue
+    must buf[2] & 128 == 0, 'Question byte is', buf[2]
     buf[2] = 128  # Response.
     buf[3] = 0    # No recursion.
-    if buf[5] != 1:  # One question.
-      continue
-    buf[7] = 1    # One answer.
+    must buf[5] >= 1, 'Num questions is', buf[5]
+    for j in range(LEN_ONE_Q_ONE_A):
+      buf[j+4] = ONE_Q_ONE_A[j]
 
-    i = 12
-    while buf[i]:
+    i = 4 + LEN_ONE_Q_ONE_A
+    while i < BUFLEN and buf[i]:  # While not len 0, marking End Of Domain Name.
       n = buf[i]
-      say 'Domain Len', n
-      for j in range(n):
-        say 'Domain Char', buf[1+j]
+      must n < 65, 'Weird Domain Length Byte:', buf[i], 'at:', i
       i += n + 1
-    say 'Domain END'
     i += 5  # Skip END, Type2, Class2.
 
     i = Put2(i, buf, (192 << 8) | 12 )  # Point to byte 12.
@@ -42,14 +50,22 @@ def ServeAnswerOnPort(answer, p):
 
     i = Put2(i, buf, 4)  # Len4 = 4
     buf[i], buf[i+1], buf[i+2], buf[i+3] = quad
-    buf = buf[:i+4]
+    i += 4
+    buf = buf[:i]
 
     say conn.WriteToUDP(buf, addr)
+    hexdump.HexDump(buf[:i], 'ANSWER to %q' % str(addr))
+
+  except as e:
+    say 'Caught Exception', e
 
 def Put2(i, buf, x):
   buf[i] = 255 & (x >> 8)
   buf[i+1] = 255 & x
   return i+2
 
+QUAD = flag.String('quad', '127.0.0.1', 'Dotted quad for answer to A record.')
+PORT = flag.Int('port', 53, 'UDP port to answer on.')
 def main(arg):
-  ServeAnswerOnPort(arg[0], int(arg[1]))
+  arg = flag.Munch(arg)
+  ServeAnswerOnPort(QUAD.X, PORT.X)
