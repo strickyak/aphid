f5b3a89d9535efd3d0a0de134ffd17d325dc290d
strick <strick@localhost>
2014-10-24 20:13:45 -0700

    flag enhancements. Triples. More consistent defaults.

--- flag.py
+++ flag.py
@@ -1,8 +1,36 @@
+"""
+Package flag parses command line flags, similar to golang's flag package.
+
+Three kinds of command line arguments are understood:
+
+  * "dash flags", like "--flag_test_b" or "--flag_test_s=foo".
+    You can omit "=value" and the value will be "1" or 1 or True.
+    The same flag should not be repeated (or last value wins).
+
+  * "triples", which are like "::name::key::value".
+    name and key cannot be empty; value can be empty.
+    These are for things where repeated dash flags would be nice.
+
+  * All other arguments.
+
+Usage:
+See flag_test_b, flag_test_i, & flag_test_s below for how
+to declare them.
+See "Munch" inside main() for how to process them.
+
+The value of a flag is in the .X field of the object.
+
+Example shell command:
+$ rye run flag.py -- --flag_test_b --flag_test_i=-44  --flag_test_s=fubar   ::a::b::c ::a::d::e ::b::x::y  opposable thumb 
+
+"""
 from go import regexp
 
 Flags = {}
+Triples = {}
 
 FLAG_RE = regexp.MustCompile('^[-][-]?([A-Za-z0-9_]+)([=](.*))?$')
+TRIPLE_RE = regexp.MustCompile('^[:][:]([A-Za-z0-9_]+)[:][:](.+?)[:][:](.*)$')
 
 def Munch(args):
   # Consume flags beginning with '-' or '--'.
@@ -17,8 +45,17 @@ def Munch(args):
     if valued:
       f.Set(value)
     else:
-      f.SetEnabled()
+      f.Set()
     args = args[1:]
+
+  while args:
+    m = TRIPLE_RE.FindStringSubmatch(args[0])
+    if not m:
+      break
+    _, name, k, v = m
+    t = Triples.setdefault(name, {})
+    t[k] = v
+    args.pop(0)
 
   # Consume '--' as a flags-stopper.
   if args and args[0] == '--':
@@ -26,7 +63,6 @@ def Munch(args):
 
   # Return the remaining args.
   return args
-
 
 class String:
   def __init__(name, dflt, doc):
@@ -39,12 +75,8 @@ class String:
   def Value():
     return .X
   
-  def Set(x):
+  def Set(x="1"):
     .X = str(x)
-  
-  def SetEnabled(x):
-    raise 'String-typed flag needs a value', .name
-
 
 class Int:
   def __init__(name, dflt, doc):
@@ -57,12 +89,8 @@ class Int:
   def Value():
     return .X
   
-  def Set(x):
+  def Set(x=1):
     .X = int(x)
-  
-  def SetEnabled(x):
-    raise 'Int-typed flag needs a value', .name
-  
 
 class Bool:
   def __init__(name, dflt, doc):
@@ -75,10 +103,20 @@ class Bool:
   def Value():
     return .X
   
-  def Set(x):
+  def Set(x=True):
     .X = bool(x)
   
-  def SetEnabled(x):
-    .X = True
+flag_test_b = Bool('flag_test_b', False, 'Test flag for Bool')
+flag_test_i = Int('flag_test_i', 0, 'Test flag for Int')
+flag_test_s = String('flag_test_s', '', 'Test flag for String')
 
+def main(argv):
+  "Show me the flags and triples."
+  argv = Munch(argv)
+  for k, v in sorted(Flags.items()):
+    print '--%s = %v' % (k, v.X)
+  for n, t in sorted(Triples.items()):
+    for k, v in sorted(t.items()):
+      print '%s [ %s ] = %s' % (n, k, v)
+  print 'REMAINDER:', argv
 pass
