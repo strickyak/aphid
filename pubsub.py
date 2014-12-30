5262067d8e59cb737c847a720c40f29a58b16d1e
strick <strick@localhost>
2014-12-30 18:39:21 -0500

    pubsub.py

---
+++ pubsub.py
@@ -0,0 +1,42 @@
+from go import regexp, sync
+
+Subs = {}
+Mu = go_new(sync.Mutex)
+
+def Subscribe(sub):
+  Mu.Lock()
+  with defer Mu.Unlock():
+    if sub.key1 not in Subs:
+      Subs[sub.key1] = {}
+    Subs[sub.key1][str(sub)] = sub
+
+def Unsubscribe(sub):
+  Mu.Lock()
+  with defer Mu.Unlock():
+    del Subs[sub.key1][str(sub)]
+
+def Publish(thing):
+  Mu.Lock()
+  with defer Mu.Unlock():
+    d = Subs[thing.key1]
+    for sub in d.values():
+      if sub.re2.FindString(thing.key2):
+         go sub.fn(thing)
+
+class Sub:
+  def __init__(key1, re2, fn):
+    .key1 = key1
+    .re2 = regexp.MustCompile(re2)
+    .fn = fn
+    .str = repr(self)
+
+  def __str__():
+    return .str
+
+class Thing:
+  def __init__(key1, key2, props):
+    .key1 = key1
+    .key2 = key2
+    .props = props
+
+pass  
