7a4124c2d614c177143479e80420877801832717
strick <strick@localhost>
2015-01-03 01:09:13 -0500

    prod.push.sh takes multiple names.

--- prod.push.sh
+++ prod.push.sh
@@ -1,12 +1,16 @@
 set -vex
-test -n "$1"
-H="$1.yak.net"
 
-ssh root@$H "killall amain || echo None"
+for x
+do
+  test -n "$x"
+  H="$x.yak.net"
 
-ssh root@$H "mkdir -p /opt/aphid"
-scp prod.one.sh root@$H:/opt/aphid/
-rsync -av amain/amain root@$H:/opt/aphid/amain
-rsync -av b.one/ root@$H:/opt/aphid/b.one/
+  ssh root@$H "killall amain || echo None"
 
-ssh -n root@$H "nohup sh /opt/aphid/prod.one.sh </dev/null >/tmp/prod.one.log 2>&1 & sleep 1"
+  ssh root@$H "mkdir -p /opt/aphid"
+  scp prod.one.sh root@$H:/opt/aphid/
+  rsync -av amain/amain root@$H:/opt/aphid/amain
+  rsync -av b.one/ root@$H:/opt/aphid/b.one/
+
+  ssh -n root@$H "nohup sh /opt/aphid/prod.one.sh </dev/null >/tmp/prod.one.log 2>&1 & sleep 1"
+done
