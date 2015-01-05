c1f515fcfaa4e0a2d0f46aa85a9307f9e636a499
strick <strick@localhost>
2015-01-05 00:08:32 -0500

    work on table.Table

--- table.py
+++ table.py
@@ -1,47 +1,82 @@
-from go import bufio
-from go import os
-from go import path/filepath
+from go import bufio, fmt, os, time
+from go import path/filepath as F
 from . import skiplist
 
 class Table:
-  # Construct with dirpath where the t.* files are, and they will be loaded.
-  def __init__(dirpath):
-    .dirpath = dirpath
-    .ts = None
-    .w = None
-    .d = skiplist.SkipList()
+  # Construct with dpath where the t.* files are, and they will be loaded.
+  def __init__(dpath, suffix='0'):
+    .dpath = dpath
+    .suffix = suffix
+    .sk = skiplist.SkipList()
     .loadDir()
 
+  def Stamp():
+    return '%d.%s' % (time.Now().Unix() // 1000000, .suffix)
+
+  def PrepareToWrite():
+    if not .w:
+      filename = F.Join(.dpath, 't.%s' % .Stamp())
+      .fd = os.Create(filename)
+      .w = bufio.NewWriter(.fd)
+
+  def ItemsWithPrefix(prefix):
+    it = .sk.Items(prefix)
+    for k, (ts, val) in it:
+      if k.startswith(prefix):
+        yield k, val
+      else:
+        return
+
+  def Get(k):
+    rec = .sk.Get(k)
+    if rec:
+      ts, val = rec
+      return val
+    else:
+      return None
+
+  def Set(k, v):
+    .PrepareToWrite()
+    ts = .Stamp()
+    .sk.Set(k, (ts, v))
+    if v is None:
+      print >>.w, '-%s\t%s\n' % (ts, k)
+    else:
+      print >>.w, '+%s\t%s\t%s\n' % (ts, k, v)
+    print >>.w, ';\n'
+    .w.Flush()
+
   def loadDir():
-    for g in filepath.Glob(filepath.Join(.dirpath,'t.*')):
+    for g in F.Glob(F.Join(.dpath,'t.*')):
       .slurpFile(g)
 
   def slurpFile(filename):
-    hold = []
+    pending = []
     for line in ReadFileLines(filename):
       if line.startswith(';'):
-        # Semicolon lines commit the hold.
-        .addem(hold)
-        hold = []
+        # Semicolon lines commit the pending records.
+        .addem(pending)
+        pending = []
       elif line.startswith('+'):
-        vec = line.split()
-        if len(vec) == 3:
-          hold.append(vec)
-        elif len(vec) == 2:
-          vec.append(False)
-          hold.append(vec)
+        vec = line.split('\t')
+        must len(vec) == 3
+        pending.append(vec)
+      elif line.startswith('-'):
+        vec = line.split('\t')
+        must len(vec) == 2
+        vec.append(None)  # Add None as third thing.
+        pending.append(vec)
         
   def addem(hold):
     for ts, k, v in hold:
+      ts = ts.lstrip('+-')  # Omit the '+' or '-'.
+
       # Look for ts of existing record with key k.
       # Remember that ts are strings, not numbers.
-      p = .d.Get(k)
-      t = p[0] if p else ''
-      if ts >= t:
-        if v:
-          .d.Set(k, (ts, v))
-        else:
-          .d.Remove(k)
+      rec = .sk.Get(k)
+      old_ts = rec[0] if rec else ''
+      if ts >= old_ts:
+        .sk.Set(k, (ts, v))
 
 def ReadFileLines(filename):
   r = os.Open(filename)
@@ -51,26 +86,26 @@ def ReadFileLines(filename):
       yield sc.Text()
     sc.Err() # Raise error, if any.
 
-def WriteFileLines(filename, it):
-  fd = os.Create(filename)
-  with defer fd.Close():
-    bw = bufio.NewWriter(fd)
-    with defer bw.Flush():
-      for line in it:
-        bw.WriteString(line)
-        bw.WriteByte('\n')
-
 class LineFileWriter:
   def __init__(filename):
     .fd = os.Create(filename)
-    .bw = bufio.NewWriter(.fd)
-  def WriteString(s):
-    .bw.WriteString(s)
-    .bw.WriteByte('\n')
+    .w = bufio.NewWriter(.fd)
+  def Printf(*args):
+    fmt.Fprintf(.w, *args)
   def Flush():
-    .bw.Flush()
+    .w.Flush()
   def Close():
-    .bw.Flush()
+    .w.Flush()
     .fd.Close()
+  #def WriteString(s):
+  #  .w.WriteString(s)
+  #  .w.WriteByte('\n')
 
-pass
+#def WriteFileLines(filename, it):
+#  fd = os.Create(filename)
+#  with defer fd.Close():
+#    bw = bufio.NewWriter(fd)
+#    with defer bw.Flush():
+#      for line in it:
+#        bw.WriteString(line)
+#        bw.WriteByte('\n')
---
+++ table_test.py
@@ -0,0 +1,34 @@
+from go import os
+from go import path/filepath as F
+from . import table
+
+D ='./__test__skiplist__'
+
+try:
+  os.RemoveAll(D)
+except:
+  pass
+
+os.Mkdir(D, 0700)
+
+fd = os.Create(F.Join(D, 't.001'))
+print >>fd, '''
+# comment
++0012345.0\tcolor\tred
++0012345.0\tflavor\tlime
++0012345.0\tsize\tXL
+;
+# overrides
++0024680.X\tcolor\tpurple
+;
+# does not override, too old.
++0012345.!\tflavor\tdurian
+;
+'''
+fd.Close()
+
+t = table.Table(D)
+must 'purple' == t.Get('color')
+must 'lime' == t.Get('flavor')
+must 'XL' == t.Get('size')
+must t.Get('bogus') == None
