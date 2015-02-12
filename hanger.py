4898bcee4935d78eedc727edfe6cc15bb6146b64
strick <strick@localhost>
2015-02-12 00:25:43 -0800

    Hanger, to hold objects while RPCs come to them.

---
+++ hanger.py
@@ -0,0 +1,64 @@
+"""You can hang an object on a Hanger, with a key name.
+   Use it within a minute, or it will be .Dispose()d.
+"""
+from go import sync, time
+from lib import sema
+from . import A
+
+TIMEOUT = 60
+
+class Hung:
+  def __init__(hanger, id, x):
+    .mu = go_new(sync.Mutex)
+    .hanger = hanger
+    .id = id
+    .x = x
+    .seq = 1
+    .Touch()
+    go .GC()
+
+  def Touch():
+    .ts = time.Now().Unix()
+
+  def GC():
+    while True:
+      A.Sleep(TIMEOUT / 2.0)
+      .mu.Lock()
+      with defer .mu.Unlock():
+        now = time.Now().Unix()
+        if now > .ts + TIMEOUT:
+          # Expired.
+          x = .x
+          .x = None
+          del .hanger.d[.id]
+          try:
+            x.Dispose()
+          except:
+            pass
+          return
+
+  def Invoke(seq, msg, *args, **kw):
+    .mu.Lock()
+    with defer .mu.Unlock():
+      must seq == .seq
+      .seq += 1
+      fn = getattr(.x, msg)
+      must callable(fn), fn
+      z = fn(*args, **kw)
+      .Touch()
+      return z
+
+class Hanger:
+  def __init__():
+    .d = {}
+
+  def Hang(obj):
+    "Hang the object on the hanger, and get an id for it."
+    id = .c.Next()
+    .d[id] = Hung(self, id, obj)
+    return id
+
+  def Invoke(id, seq, msg, *args, **kw):
+    "Invoke msg on object id.  seq starts at 1, and must increment."
+    hung = .d[id]
+    return hung.Invoke(seq, msg, *args, **kw) 
