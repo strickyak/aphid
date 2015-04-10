e34632f126ee23018c311140eefeef64beecba78
strick <strick>
2015-04-10 11:35:22 -0700

    markdown_demo.py

---
+++ markdown_demo.py
@@ -0,0 +1,12 @@
+from . import markdown
+
+def main(args):
+  for a in args:
+    say a
+    fd = open(a)
+    with defer fd.close():
+      t = fd.read()
+      f, h = markdown.Process(t)
+      say f
+      say h
+      say '############################'
