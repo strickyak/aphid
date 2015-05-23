5d6228063a7bdda2609b2633afb445d0d2c394cc
strick <strickyak@users.noreply.github.com>
2015-05-23 16:51:41 -0700

    Chucl calculations in Laph

--- laph2.py
+++ laph2.py
@@ -23,19 +23,95 @@ class LazyEval:
     .root = root
     .memo = {}
 
+  def visitCommand(p, past, future, **kw):
+    must not future
+
+    must type(p.vec) is list
+    cmd = p.vec.pop(0)
+    must type(cmd) is Bare
+    cmd = cmd.a
+
+    # First look for special forms.
+    switch cmd:
+      case 'if':
+        raise 'TODO'
+      case 'fn':
+        return p
+
+    args = [a.visit(self, past=past, future='') for a in p.vec]
+
+    # Special code for the many binary operators.
+    if len(args) == 2:
+      x, y = args
+      switch cmd:
+        case '++':
+          return x + y
+        case '+':
+          return str(float(x) + float(y))
+        case '-':
+          return str(float(x) - float(y))
+        case '*':
+          return str(float(x) * float(y))
+        case '/':
+          return str(float(x) / float(y))
+        case '//':
+          return str(float(x) // float(y))
+        case '%':
+          return str(float(x) % float(y))
+        case '<':
+          return str(float(x) < float(y))
+        case '<=':
+          return str(float(x) <= float(y))
+        case '==':
+          return str(float(x) == float(y))
+        case '!=':
+          return str(float(x) != float(y))
+        case '>':
+          return str(float(x) > float(y))
+        case '>=':
+          return str(float(x) >= float(y))
+        case 'split':
+          return y.split(x)  # (split ch str)
+        case 'join':
+          return x.join(y)   # (join sep strs)
+
+        # String comparison
+        case 'lt':
+          return str(x < y)
+        case 'le':
+          return str(x <= y)
+        case 'eq':
+          return str(x == y)
+        case 'ne':
+          return str(x != y)
+        case 'gt':
+          return str(x > y)
+        case 'ge':
+          return str(x >= y)
+
+    # Fall through for other cases.
+    switch cmd:
+      case '++':
+        return ''.join([x for x in args])
+
+      case '+':
+        return str(sum([float(x) for x in args]))
+      case '*':
+        return reduce((lambda a,b: a*b), [float(x) for x in args])
+
+    raise 'No such Chucl command: %q (or wrong number of args: %d)' % (cmd, len(args))
+
   def Eval(path, origin='/'):
     if not path.startswith('/'):
       path = P.Join('/', origin, path)
       return .Eval(path)
 
-    say 'Eval:', (path, origin)
     steps = [x for x in path.split('/') if x]
 
     return .root.visit(self, past=[], future=steps)
 
   def visitShadow(p, past, future, **kw):
     past, future = past.copy(), future.copy()
-    say 'visitShadow:', past, future, p.name, kw
     sup = kw.get('sup')
     must sup, 'Cannot shadow if not derived', past, future, p.name, kw
 
@@ -45,33 +121,24 @@ class LazyEval:
         x = future.pop(0)
         past.append(x)
 
-        say 'Shadow/Uno', past, x, future
         ret = p.diff.dic[x].visit(self, past=past, future=future, sup=P.Join(sup, x))
-        say ret
         return ret
       else:
         ret = .Eval(P.Join(sup, x), P.Join('/', *past))
-        say ret
         return ret
         
     else: # Terminating here.  Return list of things.
       sup = kw.get('sup')
-      say 'Shadow/Tres', past, sup
       must sup, (past, sup)
-      say sup, P.Join('/', *past)
       subs = .Eval(sup, P.Join('/', *past))
-      say subs
       z = dict([(e, True) for e in subs])
-      say z
 
-      say p.diff.dic
       for k in p.diff.dic:
         z[k] = True
       return sorted(z.keys())
 
   def visitDerive(p, past, future, **kw):
     past, future = past.copy(), future.copy()
-    say 'visitDerive:', past, future, p.name, kw
 
     if future:  # Going through us.
       x = future[0]
@@ -79,14 +146,10 @@ class LazyEval:
         x = future.pop(0)
         past.append(x)
 
-        say 'Derive/Uno', past, x, future, p.template
         ret = p.diff.dic[x].visit(self, past=past, future=future, sup=P.Join(p.template, x))
-        say ret
         return ret
       else:
-        say 'Derive/Dos', p.template, future, past
         ret = .Eval(P.Join(p.template, *future), P.Join('/', *past))
-        say ret
         return ret
         
     else: # Terminating here.  Return list of things.
@@ -103,7 +166,6 @@ class LazyEval:
     if future:
       x = future.pop(0)
       past.append(x)
-      say past, x, future
       y = p.dic.get(x)
       if y:
         return y.visit(self, past=past, future=future)
@@ -114,9 +176,17 @@ class LazyEval:
         
   def visitBare(p, past, future, **kw):
     if future:
-        raise "Ain't no %q in Scalar %q" % (future, past)
+        raise "Ain't no %q in Scalar at %q" % (future, past)
     else:
       return p.a
+
+  def visitDollar(p, past, future, **kw):
+    if future:
+        raise "Ain't no %q in Dollar at %q" % (future, past)
+    else:
+      return .Eval(p.a, past)
+
+##############################  AST Nodes
 
 class Derive:  # Derive from a tuple.
   def __init__(template, diff):
@@ -166,6 +236,7 @@ class Tuple: # Named tuple with {...}.
     return '{"%s" %s }' % (str(.name), ' ; '.join(['%v = %v' % (k, v) for k, v in sorted(.dic.items())]))
   def __repr__():
     return '{"%s" %s }' % (str(.name), ' ; '.join(['%v = %v' % (k, v) for k, v in sorted(.dic.items())]))
+
 class Dollar: # Dollar substituted bareword.
   def __init__(a):
     .a = a
@@ -173,6 +244,7 @@ class Dollar: # Dollar substituted bareword.
     return w.visitDollar(self, **kw)
   def __str__():
     return '$%v' % .a
+
 class Bare:  # Bareword has its own value.
   def __init__(a):
     .a = a
@@ -180,13 +252,14 @@ class Bare:  # Bareword has its own value.
     return w.visitBare(self, **kw)
   def __str__():
     return '%v' % .a
-class Parens:  # Parenthesized evaluation.
+
+class Command:  # Parenthesized command.
   def __init__(vec):
     .vec = vec
   def visit(w, **kw):
-    return w.visitParens(self, **kw)
+    return w.visitCommand(self, **kw)
   def __str__():
-    return '( %v )' % .a
+    return '( %v )' % .vec
 
 class Parse:
   def __init__(program):
@@ -198,25 +271,36 @@ class Parse:
 
   def next():
     .i += 1
-    print 'NEXT{{{ %v }}}' % .p[.i]
+    #print 'NEXT{{{ %v }}}' % .p[.i]
+
+  def command():
+          z = []
+          while .p[.i][0] != ')':
+            word, wordl = .p[.i]
+            must not Special(word)
+            if word.startswith('$'):
+              z.append( Dollar(word[1:]) )
+            else:
+              z.append( Bare(word) )
+            .next()
+          .next()
+          return Command(z)
 
   def expr():
     .d += 1
-    say '<<<', (.d, .i), .p[.i]
+    #say '<<<', (.d, .i), .p[.i]
     z = .expr2()
-    say '>>>', (.d, .i), .p[.i], str(z)
+    #say '>>>', (.d, .i), .p[.i], str(z)
     .d -= 1
     return z
+
   def expr2():
     x, xl = .p[.i]
     .next()
     if Special(x):
       switch x:
         case '(':
-          z = []
-          while .p[.i][0] != ')':
-            z.append(.expr())
-          return Parens(z)
+          return .command()
         case '{':
           d = {}
           while .p[.i][0] != '}':
@@ -247,7 +331,7 @@ class Parse:
       peek, peekl = .p[.i]
       if peek == ';' or peek == '}':
         if x.startswith('$'):
-          return Dollar(x)
+          return Dollar(x[1:])
         else:
           return Bare(x)
       elif peek == '{':
