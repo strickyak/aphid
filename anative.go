ab836adfe060ebdfd9df2896ded9d5fae3538505
strick <strickyak@users.noreply.github.com>
2015-10-11 00:36:44 -0700

    add toml to anative

--- anative.go
+++ anative.go
@@ -2,16 +2,17 @@ package aphid
 
 import (
 	// External requirements.
+	_ "github.com/BurntSushi/toml"
 	_ "github.com/gdamore/mangos"
+	_ "github.com/huandu/skiplist"
 	_ "github.com/microcosm-cc/bluemonday"
 	_ "github.com/russross/blackfriday"
-	_ "github.com/huandu/skiplist"
 )
 
 import (
 	// Internal requirements.
-	_ "github.com/strickyak/rye"
 	_ "github.com/strickyak/redhed"
+	_ "github.com/strickyak/rye"
 )
 
 import (
