5c7183d4651458e076cdecdd5d23a314f0f3b52c
strick <strick@yak.net>
2015-11-27 15:00:52 -0800

    optional pprof for lupin.

--- run-lupin.sh
+++ run-lupin.sh
@@ -1,2 +1,2 @@
 L=${L:-node1}
-pp rye run aphid.py --ring=lupin.ring --admin_init_pw=/admin/ --seeddir=lupin-seed/ "$@" lupin.laph:job:local:$L
+pp rye $PPROF run aphid.py --ring=lupin.ring --admin_init_pw=/admin/ --seeddir=lupin-seed/ "$@" lupin.laph:job:local:$L
