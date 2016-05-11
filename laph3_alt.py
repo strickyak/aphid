92bf912ca217b7eacdf951f989d435f1b7eef4a0
strick <strick@yak.net>
2016-05-11 14:56:36 -0700

    Basic enhancement works.

--- laph3_alt.py
+++ laph3_alt.py
@@ -8,7 +8,7 @@ class AltVisitor:
     .top = .compiled.tree
     .mem = {}
 
-  def visitTuple(p, here, togo):
+  def visitTuple(p, here, togo, shadow, stops):
     # dic
     h, t = L.HT(togo)
     if not h:
@@ -21,10 +21,11 @@ class AltVisitor:
     say here, togo, h, t, x
     if x is NONE:
       return ('bad', 'visitTuple cannot find %q in %q' % (h, here))
-    
-    return x.visit(self, here=hereH, togo=t)
-        
-  def visitDerive(p, here, togo):
+
+    shadow = L.J(shadow, h) if shadow else shadow
+    return x.visit(self, here=hereH, togo=t, shadow=shadow, stops=stops)
+
+  def visitDerive(p, here, togo, shadow, stops):
     # template, diff
 
     d = .LookupDir(p.template, L.D(here))
@@ -40,29 +41,51 @@ class AltVisitor:
     hereH = L.J(here, h)
     x = p.diff.get(h, NONE)
     if x is NONE:
-      z = .EvalPath(L.J(absT, h, t))
+      z = .EvalPath(L.J(absT, h, t))  # Add real path.
       say here, togo, z
       return z
-    
-    z = x.visit(self, here=hereH, togo=t)
+
+    # Shadow begins here, when we go down the diff path.
+    z = x.visit(self, here=hereH, togo=t, shadow=L.J(absT, h), stops=stops)
     say here, togo, z
     return z
 
-  def visitEnhance(p, here, togo):
+  def visitEnhance(p, here, togo, shadow, stops):
     # dslot, diff
-    raise 'TODO'
+    h, t = L.HT(togo)
+    say here, togo, shadow, stops, h, t
+    if not h:
+      # Just the enhanced directory.
+      enhancedSet = set(p.diff.keys())
+      shadowSet = .EvalPath(shadow, stops=stops)
+      say here, togo, shadow, stops
+      say enhancedSet
+      say shadowSet
+      must type(shadowSet) is set
+      return enhancedSet | shadowSet  # Union.
 
-  def visitBare(p, here, togo):
+    hereH = L.J(here, h)
+    x = p.diff.get(h, NONE)
+    if x is NONE:
+      z = .EvalPath(L.J(shadow, h, t))  # to be added: real path.
+      say here, togo, shadow, h, t, z
+      return z
+
+    z = x.visit(self, here=hereH, togo=t, shadow=L.J(shadow, h), stops=stops)
+    say here, togo, shadow, h, t, z
+    return z
+
+  def visitBare(p, here, togo, shadow, stops):
     must not togo, here, togo
     .mem[here] = p.a
     return p.a
 
-  def visitCommand(p, here, togo):
+  def visitCommand(p, here, togo, shadow, stops):
     raise 'TODO'
 
-  def EvalPath(path):
-    return .visitTuple(.top, here='/', togo=path)
-    
+  def EvalPath(path, shadow=None, stops=None):
+    return .visitTuple(.top, here='/', togo=path, shadow=shadow, stops=stops)
+
   def LookupDir(path, cwd):
     if path.startswith('/'):
       return '/'
@@ -134,6 +157,7 @@ t3 = L.Compile(`
     N = { c = 333 ; d = 444 }
   }
   Y = X {
+    M { a = 555 ; f = 666 }
     P = { z = 888 }
   }
 `)
@@ -142,7 +166,17 @@ av3 = AltVisitor(t3)
 must av3.EvalPath('/') == set(['X', 'Y'])
 must av3.EvalPath('/X') == set(['M', 'N'])
 must av3.EvalPath('/Y') == set(['M', 'N', 'P'])
+must av3.EvalPath('/X/M/a') == '111'
+must av3.EvalPath('/X/M/b') == '222'
+must av3.EvalPath('/X/N/c') == '333'
+must av3.EvalPath('/X/N/d') == '444'
 
+must av3.EvalPath('/Y/M') == set(['a', 'b', 'f'])
+must av3.EvalPath('/Y/M/a') == '555'
+must av3.EvalPath('/Y/M/b') == '222'
+must av3.EvalPath('/Y/M/f') == '666'
+must av3.EvalPath('/Y/N/d') == '444'
+must av3.EvalPath('/Y/P/z') == '888'
 
 ################################
 
